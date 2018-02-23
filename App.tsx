369e67d600ca0af593ba8d5645c7c1fa49e856b2
Antonio Barandica <anjose121@gmail.com>
2018-02-23 16:01:54 -0500

    add search page

--- App.tsx
+++ App.tsx
@@ -3,32 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import {
   StackNavigator,
 } from 'react-navigation';
-
-class SearchPage extends React.Component<{}> {
-
-  static navigationOptions = {
-    title: 'Property Finder',
-  };
-  
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.ts to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import SearchPage from './src/SearchPage';
 
 const App = StackNavigator({
   Home: { screen: SearchPage},
